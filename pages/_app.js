f438327f157a160c0f435f50a028cf5a04239bb0
Nhat Nguyen <newbie.dev.js@gmail.com>
2022-08-01 00:28:09 +0700

    add animation in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,13 @@ import Menu from "@/widgets/menu/index";
 import Footer from "@/widgets/footer/";
 import SliderBar from "@/widgets/slider-bar/index";
 import ProgressBar from "@badrap/bar-of-progress";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import GlobalContext from "../context/global";
 import "@/shared/styles/globals.css";
 import { SWRConfig } from "swr";
 import { timeCache } from "@/shared/config";
 import { fetcherClient } from "api-client/base/axios-client";
+import { motion } from "framer-motion";
 
 const progress = new ProgressBar({
   size: 5,
@@ -22,6 +23,7 @@ const progress = new ProgressBar({
 // Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <SWRConfig
       value={{
@@ -32,9 +34,17 @@ function MyApp({ Component, pageProps }) {
       <GlobalContext data={pageProps}>
         <Header />
         <Menu />
+
         <main className="container mx-auto flex mt-10">
           <div className="w-9/12">
-            <Component {...pageProps} />
+            <motion.div
+              key={router.pathname}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ ease: "easeOut", duration: 0.3 }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
           </div>
           <div className="w-3/12 ml-3">
             <SliderBar />
